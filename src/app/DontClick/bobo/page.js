e2cffeeec40dd3aeae7ae75ecaf0c0cc0c189647
Marvell Ayon <loewiayon12@gmail.com>
2024-06-10 14:35:09 +0800

    test

--- src/app/DontClick/bobo/page.js
+++ src/app/DontClick/bobo/page.js
@@ -4,7 +4,7 @@ const page = () => {
   return (
     <div>
         <img className='w-full h-[1000px]' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRk05x2D98ZVOlokR1oAHwfFhjjqwHSfVEkcioA_v9jwg&s" alt="" />
-        
+        sadfasdf
     </div>
   )
 }
