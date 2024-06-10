ef88d4ae9a8849eef94ea632b4f63d97386c8b60
Marvell Ayon <loewiayon12@gmail.com>
2024-06-10 18:42:09 +0800

    fixed some issues

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-
-const page = () => {
-  return (
-    <div className="h-screen">
-        {/* <img className='w-full h-[1000px]' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRk05x2D98ZVOlokR1oAHwfFhjjqwHSfVEkcioA_v9jwg&s" alt="" /> */}
-    <div className="flex h-screen justify-center items-end">
-    <a href="/About/loobwarning/2warning/3waning" className='flex justify-center items-center bg-black text-red-500 p-3 w-[500px] h-[100px] rounded-full'>KULIT MO!!!</a>
-    </div>
-    </div>
-  )
-}
-
-export default page
-=======
 "use client";
 import React, { useState } from "react";
 
@@ -31,4 +15,3 @@ export default function Home() {
     </div>
   );
 }
->>>>>>> 43490ab87e08e700b007ff706e6d5fcf712ea249
